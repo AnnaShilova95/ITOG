37523ef622b145a0825b35e4c6cda60cbd8f7db9
AnnaShilova <Anna.Shilova@spcpu.ru>
2022-10-02 21:17:53 +0300

    3rd commit

--- Program.cs
+++ Program.cs
@@ -4,43 +4,48 @@
 //либо задать на старте выполнения алгоритма. 
 //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-string [] array1 = {"Hello", "2", "World", ":-)"};
-string [] array2 = {"1234", "1567", "-2", "computer science"};
-string [] array3 = {"Russia", "Denmark", "Kazan"};
+//Задаем исходный массив
 
-maxLenght = 3;
+string[] array1 = {"Hello", "2", "world", ":-)"};
 
-string [] ShortArray (string [] array){
-    int lenght = array.Length;
-    string [] arrResult = new string[length];
-    int count = 0;
-
-    for (int i = 0; i < length; i++){
-        if(array[i].Length <= maxLengnt){
-           arrResult[count] = array[i];
-		   count++; 
-        }
-    }
-    return arrResult;
+//Метод, укорачивающий исходный массив до <= 3х элементов и переносящий элементы в новый
+		
+string[] ShortArray(string[] array){
+	int length = array.Length;			
+	string[] result = new string[length];
+	int count = 0;
+			
+	for (int i = 0; i < length; i++){
+		if(array[i].Length <= 3){
+			result[count] = array[i];
+			count++;
+			}
+		}
+	return result;
 }
+		
+//Вывод масиива на экран
 
 void PrintArray(string[] array){
-	if(array.Length == 0){
-		Console.WriteLine("Array is empty");
-	}
-	else{
-       for (int i = 0; i < array.Length; i++){
-        Console.Write($"{array[i]} ");
+	if(array.Length == 0) Console.WriteLine("Array is empty");
+    else{
+	Array.ForEach(array, (str) => Console.Write($"{str} "));
+	Console.WriteLine();
     }
-    Console.WriteLine();
-    }
-} 
+}
+	
+//Вывод исходного и итогового массивов
 
-void PrintOrigArray (string[] array){
-	Console.WriteLine("Начальный массив:");
+void PrintOrigArr(string[] array){
+    Console.WriteLine("Изначальный массив:");
 	PrintArray(array);
-	string[] shortArray = ShortArray (array);
-	Console.WriteLine($"Итоговый массив");
+	string[] shortArray = ShortArray(array);
+	Console.WriteLine("Итоговый массив");
 	PrintArray(shortArray);
 	Console.WriteLine();
 }
+		
+PrintOrigArr(array1);
+
+
+	
